8f4da2abbb7317ad77242d2ffe123769a8dc3990
DELL <DELL@Nutan>
2025-10-30 23:24:25 +0530

    Spring Boot Project Commit

---
+++ BootMsProj13-IPLTeamMS-ProviderMs-MiniProject/src/main/java/com/nt/service/IPLTeamMgmtService.java
@@ -0,0 +1,12 @@
+package com.nt.service;
+
+import java.util.List;
+
+import com.nt.entity.IPLTeam;
+import com.nt.vo.IPLTeamVO;
+
+public interface IPLTeamMgmtService {
+     public  String registerIPLTeam(IPLTeamVO teamVO);
+     public   List<IPLTeamVO> getAllTeams();
+     public    IPLTeamVO    getTeamById(int teamId)throws Exception;
+}
